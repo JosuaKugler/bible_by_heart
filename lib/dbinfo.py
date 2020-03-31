4829c32d8b674bca74952588844456dcb0bfbc52
JosuaKugler <josua.kugler@gmail.com>
2020-03-31 23:46:49 +0200

    changed minimum flutter sdk requirements and fixed chapter bug

--- lib/dbinfo.py
+++ lib/dbinfo.py
@@ -1,14 +1,20 @@
 import sqlite3
 import codecs
 
-conn = sqlite3.connect('bible.db')
+conn = sqlite3.connect('../assets/bible.db')
 c = conn.cursor()
 
-c.execute('''CREATE''')
+c.execute("SELECT book, chapter FROM bible WHERE verse = 1;")
 
 rows = c.fetchall()
-print(rows[0])
-print(rows[-1])
 
+bookMap = {}
+for (book, chapter) in rows:
+    try:
+        bookMap[book] += 1
+    except:
+        bookMap[book] = 1
+
+print(bookMap)
 #conn.commit()
 conn.close()
--- lib/selectpassagepage.dart
+++ lib/selectpassagepage.dart
@@ -19,8 +19,7 @@ class _SelectPassageState extends State<SelectPassage>
   final Function setNewChapter;
   final Passage displayPassage;
   var book;
-  _SelectPassageState(this.helper, this.setNewChapter, this.displayPassage)
-      : book = displayPassage.book;
+  _SelectPassageState(this.helper, this.setNewChapter, this.displayPassage);
 
   final List<Tab> myTabs = <Tab>[
     Tab(text: 'Buch'),
@@ -32,6 +31,7 @@ class _SelectPassageState extends State<SelectPassage>
   @override
   void initState() {
     super.initState();
+    book = displayPassage.book;
     _tabController = TabController(vsync: this, length: myTabs.length);
   }
 
@@ -104,6 +104,75 @@ class _SelectPassageState extends State<SelectPassage>
     "Rev": "Offenbarung",
   };
 
+  final Map<String, int> chapterNumbers = {
+    'Gen': 50,
+    'Exo': 40,
+    'Lev': 27,
+    'Num': 36,
+    'Deu': 34,
+    'Jos': 24,
+    'Jdg': 21,
+    'Rut': 4,
+    '1Sa': 31,
+    '2Sa': 24,
+    '1Ki': 22,
+    '2Ki': 25,
+    '1Ch': 29,
+    '2Ch': 36,
+    'Ezr': 10,
+    'Neh': 13,
+    'Est': 10,
+    'Job': 42,
+    'Psa': 150,
+    'Pro': 31,
+    'Ecc': 12,
+    'Sol': 8,
+    'Isa': 66,
+    'Jer': 52,
+    'Lam': 5,
+    'Eze': 48,
+    'Dan': 12,
+    'Hos': 14,
+    'Joe': 4,
+    'Amo': 9,
+    'Abd': 1,
+    'Jon': 4,
+    'Mic': 7,
+    'Nah': 3,
+    'Hab': 3,
+    'Zep': 3,
+    'Hag': 2,
+    'Zec': 14,
+    'Mal': 3,
+    'Mat': 28,
+    'Mar': 16,
+    'Luk': 24,
+    'Joh': 21,
+    'Act': 28,
+    'Rom': 16,
+    '1Co': 16,
+    '2Co': 13,
+    'Gal': 6,
+    'Eph': 6,
+    'Phi': 4,
+    'Col': 4,
+    '1Th': 5,
+    '2Th': 3,
+    '1Ti': 6,
+    '2Ti': 4,
+    'Tit': 3,
+    'Phm': 1,
+    'Heb': 13,
+    'Jam': 5,
+    '1Pe': 5,
+    '2Pe': 3,
+    '1Jo': 5,
+    '2Jo': 1,
+    '3Jo': 1,
+    'Jud': 1,
+    'Rev': 22
+  };
+
   List<ListTile> generateBookList() {
     List<String> bookList = [];
     short2long.forEach((key, value) {
@@ -113,9 +182,24 @@ class _SelectPassageState extends State<SelectPassage>
       return ListTile(
         title: Text(short2long[book]),
         onTap: () {
-          this.book = book;
-          print(book);
+          setState(() {
+            this.book = book;
+          });
           _tabController.animateTo(1);
+        },
+      );
+    }).toList();
+  }
+
+  List<ListTile> generateChapterList(String book, BuildContext context) {
+    int chapterNumber = chapterNumbers[book];
+    List<int> chapterList = [for(var i=0; i < 10; i++) i];
+    return chapterList.map((int chapter) {
+      return ListTile(
+        title: Text('$chapter'),
+        onTap: () {
+          this.setNewChapter(Passage(book, chapter, 1));
+          Navigator.pop(context);
         },
       );
     }).toList();
@@ -144,54 +228,10 @@ class _SelectPassageState extends State<SelectPassage>
         ListView(
           children: this.generateBookList(),
         ),
-        ChapterSelection(getBook, this.helper, this.setNewChapter),
+        ListView(
+          children: this.generateChapterList(this.book, context),
+        )
       ]),
-    );
-  }
-}
-
-class ChapterSelection extends StatelessWidget {
-  final Function getBook;
-  final DataBaseHelper helper;
-  final Future<int> _chapterNumber;
-  final Function setNewChapter;
-  ChapterSelection(this.getBook, this.helper, this.setNewChapter)
-      : _chapterNumber = helper.getNumberOfChapters(getBook());
-
-  List<ListTile> generateChapterList(int chapterNumber, BuildContext context) {
-    List<int> chapterList = [];
-    for (int i = 1; i < chapterNumber + 1; i++) {
-      chapterList.add(i);
-    }
-    return chapterList.map((int chapter) {
-      return ListTile(
-        title: Text('$chapter'),
-        onTap: () {
-          this.setNewChapter(Passage(this.getBook(), chapter, 1)); //doesn't work because it is built right at the beginning
-          Navigator.pop(context);
-        },
-      );
-    }).toList();
-    //add the functionality to click on a chapter and then submit the new passage and pop
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _chapterNumber,
-      builder: (context, AsyncSnapshot<int> snapshot) {
-        Widget result;
-        if (snapshot.hasData) {
-          result = ListView(
-            children: generateChapterList(snapshot.data, context),
-          );
-        } else if (snapshot.hasError) {
-          result = Text(snapshot.error);
-        } else {
-          result = Center(child: Text('Awaiting result...'));
-        }
-        return result;
-      },
     );
   }
 }
